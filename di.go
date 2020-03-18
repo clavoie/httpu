db4f162bb58fc4ec81ca8132b1c36e19bd67775d
Chris <clavoie@gmail.com>
2020-03-18 17:09:40 -0400

    Converting to go module v1.0.1 or something

--- di.go
+++ di.go
@@ -1,6 +1,6 @@
 package httpu
 
-import "github.com/clavoie/di"
+import "github.com/clavoie/di/v2"
 
 // NewDiDefs returns a new collection of di definitions that
 // can be used to inject httpu into your project.
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/clavoie/httpu
+
+go 1.14
+
+require (
+	github.com/clavoie/di/v2 v2.2.0
+	github.com/clavoie/erru v1.0.0
+	github.com/clavoie/logu v1.3.0
+	github.com/golang/mock v1.4.3
+)
